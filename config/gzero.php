bf60ca219eedcc760f574c52077bda24f5704ac3
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2015-05-23 11:41:49 +0200

    Init

---
+++ config/gzero.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'domain'       => 'dev.gzero.pl',
+    'siteName'     => 'G-ZERO CMS',
+    'multilang'    => [
+        'enabled'   => true,
+        'detected'  => false, // Do not change, changes in runtime!
+        'subdomain' => false
+    ],
+    //'upload'       => [
+    //    'path'   => public_path('uploads'),
+    //    'public' => asset('uploads')
+    //],
+    'block_type'   => [
+        'basic' => 'Gzero\Core\Handler\Block\Basic',
+        'menu'  => 'Gzero\Core\Handler\Block\Menu'
+    ],
+    'content_type' => [
+        'content'  => 'Gzero\Core\Handler\Content\Content',
+        'category' => 'Gzero\Core\Handler\Content\Category'
+    ]
+];
