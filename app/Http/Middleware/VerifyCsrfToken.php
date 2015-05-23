9dd5fc5777345d49f7ff8c828ab085b50ada6490
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2015-05-23 14:11:21 +0200

    Disabled CSRF for api calls

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -5,16 +5,21 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier {
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		return parent::handle($request, $next);
-	}
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (preg_match('/^api\./', $request->header('host'))) {
+            return $next($request);
+        } else {
+            return parent::handle($request, $next);
+        }
+    }
 
 }
