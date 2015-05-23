c5b8e491e8ac172432c783c3e1f129f1e8f3dff7
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2015-05-23 16:14:59 +0200

    Added api error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,42 +1,65 @@
 <?php namespace App\Exceptions;
 
 use Exception;
+use Gzero\Validator\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler {
 
-	/**
-	 * A list of the exception types that should not be reported.
-	 *
-	 * @var array
-	 */
-	protected $dontReport = [
-		'Symfony\Component\HttpKernel\Exception\HttpException'
-	];
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        'Symfony\Component\HttpKernel\Exception\HttpException'
+    ];
 
-	/**
-	 * Report or log an exception.
-	 *
-	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-	 *
-	 * @param  \Exception  $e
-	 * @return void
-	 */
-	public function report(Exception $e)
-	{
-		return parent::report($e);
-	}
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception $e
+     *
+     * @return void
+     */
+    public function report(Exception $e)
+    {
+        return parent::report($e);
+    }
 
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $e
-	 * @return \Illuminate\Http\Response
-	 */
-	public function render($request, Exception $e)
-	{
-		return parent::render($request, $e);
-	}
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception               $e
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $e)
+    {
+        if (preg_match('/^api\./', \Request::getHost())) {
+            if ($e instanceof ValidationException) {
+                return \Response::json(
+                    [
+                        'code'     => 500,
+                        'messages' => $e->getErrors()
+                    ],
+                    500
+                );
+            } else {
+                return \Response::json(
+                    [
+                        'code'    => $e->getCode(),
+                        'message' => $e->getMessage()
+                    ],
+                    500
+                );
+            }
+        } else {
+            return parent::render($request, $e);
+        }
+    }
 
 }
