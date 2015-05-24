00e485b8d8ea4cf4241c57bb5ec61b7f466aa620
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2015-05-24 16:38:48 +0200

    Updated routes to use functions instead of facades

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,51 +14,54 @@
 /**
  * Multi language group
  */
-Route::group(
+group(
     setMultilangRouting(),
     function () {
-        Route::group(
+        group(
             ['before' => 'auth'],
             function () {
-                Route::get('account', ['as' => 'account', 'uses' => 'AccountController@account']);
-                Route::get('account/edit', ['as' => 'account.edit', 'uses' => 'AccountController@edit']);
+                get('account', ['as' => 'account', 'uses' => 'AccountController@account']);
+                get('account/edit', ['as' => 'account.edit', 'uses' => 'AccountController@edit']);
 
-                Route::group(
+                group(
                     ['prefix' => 'api/v1'],
                     function () {
-                        Route::resource('account', 'AccountApiController', ['only' => ['show', 'update', 'destroy']]);
+                        resource('account', 'AccountApiController', ['only' => ['show', 'update', 'destroy']]);
                     }
                 );
             }
         );
 
-        Route::get('login', ['as' => 'login', 'uses' => 'UserController@login']);
-        Route::post('login', ['as' => 'post.login', 'uses' => 'UserController@postLogin']);
-        Route::get('register', ['as' => 'register', 'uses' => 'UserController@register']);
-        Route::post('register', ['as' => 'post.register', 'uses' => 'UserController@postRegister']);
-        Route::get('password/remind', ['as' => 'password.remind', 'uses' => 'UserController@remind']);
-        Route::post('password/remind', ['as' => 'post.password.remind', 'uses' => 'UserController@postRemind']);
-        Route::get('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'UserController@reset']);
-        Route::post('password/reset', ['as' => 'post.password.reset', 'uses' => 'UserController@postReset']);
-        Route::get('logout', ['as' => 'logout', 'uses' => 'UserController@logout']);
-        Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
+        get('login', ['as' => 'login', 'uses' => 'UserController@login']);
+        get('register', ['as' => 'register', 'uses' => 'UserController@register']);
+        post('login', ['as' => 'post.login', 'uses' => 'UserController@postLogin']);
+        post('register', ['as' => 'post.register', 'uses' => 'UserController@postRegister']);
+
+        get('password/remind', ['as' => 'password.remind', 'uses' => 'UserController@remind']);
+        get('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'UserController@reset']);
+        post('password/remind', ['as' => 'post.password.remind', 'uses' => 'UserController@postRemind']);
+        post('password/reset', ['as' => 'post.password.reset', 'uses' => 'UserController@postReset']);
+
+        get('logout', ['as' => 'logout', 'uses' => 'UserController@logout']);
+
+        get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 
         // dev links
-        Route::get('_dev', ['as' => '_dev', 'uses' => 'DevController@index']);
-        Route::get('_dev/emails/{email}', ['as' => '_dev.emails', 'uses' => 'DevController@emails']);
+        get('_dev', ['as' => '_dev', 'uses' => 'DevController@index']);
+        get('_dev/emails/{email}', ['as' => '_dev.emails', 'uses' => 'DevController@emails']);
 
-        Route::get('{path?}', 'ContentController@dynamicRouter')->where('path', '.*');
+        get('{path?}', 'ContentController@dynamicRouter')->where('path', '.*');
     }
 );
 
-App::make('user.menu')->addLink(URL::route('account'), 'user.my_account');
+app()->make('user.menu')->addLink(URL::route('account'), 'user.my_account');
 //App::make('user.menu')->addChild(['url' => URL::route('logout'), 'title' => 'common.logout'], URL::route('account'));
 
 
 // Laravel 5 DEFAULT ROUTES
-//Route::get('/', 'WelcomeController@index');
+//get('/', 'WelcomeController@index');
 //
-//Route::get('home', 'HomeController@index');
+//get('home', 'HomeController@index');
 //
 //Route::controllers([
 //	'auth' => 'Auth\AuthController',
