a8b84b84acb4309f42c8aef98883d3722afdefa0
Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
2015-06-11 19:01:28 +0200

    remove images from slider

--- resources/views/blocks/slider.blade.php
+++ resources/views/blocks/slider.blade.php
@@ -1,44 +1,51 @@
 <div class="slider" id="slider">
     <ul class="sequence-canvas">
-        @foreach($slides as $slide)
-            <li>
+        <li class="slide1">
+            <div class="jumbotron text-center">
+                <h1>GZERO CMS!</h1>
 
-                <div class="jumbotron text-center"  style=" background: url({{ config('gzero.upload.public').'/slides/'.$slide }});
-                        background-size: cover;
-                        background-position: center;
-                        height: 400px;">
-                    <h1>Marketing stuff!</h1>
+                <p class="lead">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
+                    justo sit amet.Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
 
-                    <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac
-                        cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-                    <p>
-                        <a class="btn btn-lg btn-success" href="{{ route('register') }}" role="button">Get started today</a>
-                    </p>
-                </div>
+                <p>
+                    <a class="btn btn-lg btn-success" href="{{ route('register') }}" role="button">Get started today</a>
+                </p>
+            </div>
 
-            </li>
-        @endforeach
+        </li>
+        <li class="slide2">
+            <div class="jumbotron text-center">
+                <h1>Marketing stuff!</h1>
+
+                <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac
+                    cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
+
+                <p>
+                    <a class="btn btn-lg btn-success" href="{{ route('register') }}" role="button">Get started today</a>
+                </p>
+            </div>
+        </li>
     </ul>
 </div>
 
 @section('footerScripts')
 
-<script src="{{ asset('/js/jquery.sequence-min.js') }}"></script>
+    <script src="{{ asset('/js/jquery.sequence-min.js') }}"></script>
 
-<script>
-    $(document).ready(function() {
+    <script>
+        $(document).ready(function() {
 
-        $(document).ready(function(){
-            var options = {
-                animateStartingFrameIn: false,
-                autoPlay: true,
-                autoPlayDelay: 5000
-            };
+            $(document).ready(function() {
+                var options = {
+                    animateStartingFrameIn: false,
+                    autoPlay: true,
+                    autoPlayDelay: 5000
+                };
 
-            var mySequence = $("#slider").sequence(options).data("sequence");
+                var mySequence = $("#slider").sequence(options).data("sequence");
+            });
+
         });
-
-    });
-</script>
+    </script>
 
 @stop
