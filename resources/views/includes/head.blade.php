f13d3815d662dc1ceb9396fa031ad85775b596ca
Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
2015-06-29 12:12:31 +0200

    seoTitle and description

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,9 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="@yield('seoDescription')">
 <title>@yield('title') - {{ config('gzero.siteName') }}</title>
+
 @section('head')
     <link href="{{ asset('/css/jquery.metisMenu.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
