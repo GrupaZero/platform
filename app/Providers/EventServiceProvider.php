20a1d83fa7643c9cb309570e5dbd3caa863fb37c
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2015-12-19 16:02:31 +0100

    Enabled BlockLoad event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -11,8 +11,8 @@ class EventServiceProvider extends ServiceProvider {
      * @var array
      */
     protected $listen = [
-        'event.name' => [
-            'EventListener',
+        'router.matched' => [
+            'Gzero\Core\Listeners\Events\BlockLoad',
         ],
     ];
 
