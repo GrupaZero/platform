37394158eb636d6bdaa0310eee639979e1a0c277
Aleksandra Augustyn <a.augustyn@grupazero.pl>
2016-08-02 22:08:26 +0200

    added google analytics userId for logged user

--- scripts/install_staging.sh
+++ scripts/install_staging.sh
@@ -3,4 +3,4 @@
 docker-compose -f staging.yml build --pull staging_web
 docker-compose -f staging.yml up --no-deps -d staging_web
 echo -e "\e[91mCleaning old docker containers\e[0m"
-docker rmi $(docker images -f "dangling=true" -q)
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
