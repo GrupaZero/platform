081011980b29669cd18eeb28306ee1c8b32645f7
michalstrzelecki <michasstrzelecki@gmail.com>
2016-08-16 11:39:41 +0200

    Remove eventTracker js file.

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -52,7 +52,6 @@
 
 @section('head')
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('/js/eventTracker.js') }}"></script>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
