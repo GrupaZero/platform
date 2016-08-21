2fcbd74fed211128bf965ad85fc37ca3511b5fb2
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2016-08-21 14:15:15 +0200

    Updated cms to 0.3.0 to use cms error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,27 +1,9 @@
 <?php namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Support\Facades\App;
-use Whoops\Run;
-use Whoops\Handler\PrettyPageHandler;
-use Gzero\Validator\ValidationException;
-use Gzero\Api\AccessForbiddenException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Gzero\Core\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler {
-
-    const VALIDATION_ERROR = 400;   // (Bad Request)
-    const SERVER_ERROR = 500;       // (Internal Server Error)
-    const FORBIDDEN_ERROR = 403;    // (Forbidden Error)
-
-    /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        \Symfony\Component\HttpKernel\Exception\HttpException::class
-    ];
 
     /**
      * Report or log an exception.
@@ -47,72 +29,6 @@ class Handler extends ExceptionHandler {
      */
     public function render($request, Exception $e)
     {
-        if (preg_match('/^api/', $request->getHost())) {
-            /** @var $CORS \Asm89\Stack\CorsService */
-            $CORS = app()->make('Asm89\Stack\CorsService');
-            if ($e instanceof ValidationException) {
-                return $CORS->addActualRequestHeaders(
-                    response()->json(
-                        [
-                            'code'    => self::VALIDATION_ERROR,
-                            'message' => ($e->getMessage()) ? $e->getMessage() : 'Validation Error',
-                            'errors'  => $e->getErrors()
-                        ],
-                        self::VALIDATION_ERROR
-                    ),
-                    $request
-                );
-            } elseif ($e instanceof AccessForbiddenException) {
-                return $CORS->addActualRequestHeaders(
-                    response()->json(
-                        [
-                            'code'    => self::FORBIDDEN_ERROR,
-                            'message' => ($e->getMessage()) ? $e->getMessage() : 'Forbidden'
-                        ],
-                        self::FORBIDDEN_ERROR
-                    ),
-                    $request
-                );
-            } else {
-                if (App::environment() == 'production') {
-                    return $CORS->addActualRequestHeaders(
-                        response()->json(
-                            [
-                                'code'    => self::SERVER_ERROR,
-                                'message' => ($e->getMessage()) ? $e->getMessage() : 'Internal Server Error',
-                            ],
-                            self::SERVER_ERROR
-                        ),
-                        $request
-                    );
-                } else {
-                    return $CORS->addActualRequestHeaders(
-                        response()->json(
-                            [
-                                'code'    => self::SERVER_ERROR,
-                                'type'    => get_class($e),
-                                'message' => ($e->getMessage()) ? $e->getMessage() : 'Internal Server Error',
-                                'file'    => $e->getFile(),
-                                'line'    => $e->getLine(),
-                            ],
-                            self::SERVER_ERROR
-                        ),
-                        $request
-                    );
-                }
-            }
-        } else {
-            if (config('app.debug')) {
-                $whoops = new Run;
-                $whoops->pushHandler(new PrettyPageHandler);
-                return response(
-                    $whoops->handleException($e),
-                    method_exists($e, 'getStatusCode') ? $e->getStatusCode() : self::SERVER_ERROR,
-                    method_exists($e, 'getHeaders') ? $e->getHeaders() : []
-                );
-            } else {
-                return parent::render($request, $e);
-            }
-        }
+        return parent::render($request, $e);
     }
 }
