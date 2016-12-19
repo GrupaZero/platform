6024f05aa839da99ec51c697c0faa7e7c64d0101
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2016-12-19 17:13:00 +0100

    Passport oauth keys path fix plus docker-compose restart policy

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,9 +1,14 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 
+use Laravel\Passport\Passport;
+
 if (file_exists(dirname(__DIR__) . '/.env.testing')) {
     (new \Dotenv\Dotenv(dirname(__DIR__), '.env.testing'))->load();
 }
+
+// We need to tell Laravel Passport where to find oauth keys
+Passport::loadKeysFrom(dirname(__DIR__) . '/vendor/gzero/testing/oauth/');
 
 \Codeception\Configuration::$defaultSuiteSettings['modules']['config']['Db'] = [
     'dsn'      => 'mysql:host=' . env('DB_HOST', 'dev_db_tests') . ';dbname=' . env('DB_DATABASE', 'gzero-cms'),
