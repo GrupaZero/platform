1a38da86ca931681a48ff3f5e56509b8ebc4fa34
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2017-01-02 20:39:03 +0100

    Unified service names between environments

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -11,7 +11,7 @@ if (file_exists(dirname(__DIR__) . '/.env.testing')) {
 Passport::loadKeysFrom(dirname(__DIR__) . '/vendor/gzero/testing/oauth/');
 
 \Codeception\Configuration::$defaultSuiteSettings['modules']['config']['Db'] = [
-    'dsn'      => 'mysql:host=' . env('DB_HOST', 'dev_db_tests') . ';dbname=' . env('DB_DATABASE', 'gzero-cms'),
+    'dsn'      => 'mysql:host=' . env('DB_HOST', 'db_tests_server') . ';dbname=' . env('DB_DATABASE', 'gzero-cms'),
     'user'     => env('DB_USERNAME', 'root'),
     'password' => env('DB_PASSWORD', ''),
     'dump'     => 'vendor/gzero/testing/db/dump.sql',
