c9faf3f25baaef81fe0cbd25d4e5b303861df7f1
Michał <michasstrzelecki@gmail.com>
2017-03-13 12:30:17 +0100

    Add some fixes. (#53)
    * Add some fixes.
    
    * Change to check if app_env equals production.

--- resources/views/includes/alternateLinks.blade.php
+++ resources/views/includes/alternateLinks.blade.php
@@ -1,6 +1,10 @@
-@if(config('gzero.multilang.enabled') && !empty($content))
-    @foreach($content->route->translations as $translation)
-        <link rel="alternate" href="{{$content->routeUrl($translation->langCode)}}"
-              hreflang="{{$translation->langCode}}"/>
-    @endforeach
+@if(config('gzero.multilang.enabled'))
+    <link rel="alternate" href="{{env('APP_URL')}}" hreflang="x-default" />
+    @if(empty($content))
+        <link rel="alternate" href="{{url()->current()}}" hreflang="{{$lang->code}}"/>
+    @else
+        @foreach($content->route->translations as $translation)
+            <link rel="alternate" href="{{$content->routeUrl($translation->lang_code)}}" hreflang="{{$translation->lang_code}}"/>
+        @endforeach
+    @endif
 @endif
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -17,7 +17,7 @@
 
 </script>
 
-@if(option('seo', 'google_analytics_id'))
+@if(option('seo', 'google_analytics_id') && env('APP_ENV') === 'production')
 <!-- Google Analytics web tracking code -->
 <script type="text/javascript">
     (function(i, s, o, g, r, a, m) {
