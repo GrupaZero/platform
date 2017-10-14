24e2f53b82611c121266ec2a457330656f47fafc
Aleksandra Augustyn <a.augustyn@grupazero.pl>
2017-10-14 18:31:27 +0200

    Exception handler from base package

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Gzero\Base\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
