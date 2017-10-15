c5a10c23d77648ad4e7fd54c35a657ae7a667d1e
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2017-10-15 11:54:46 +0200

    Fixed api auth issues

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -38,6 +38,11 @@ if (token) {
 }
 
 /**
+ * We need to pass cookie to api subdomain
+ */
+window.axios.defaults.withCredentials = true;
+
+/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
