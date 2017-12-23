31a63395b0fef64c5b985ea8d119800bc4270cde
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2017-12-23 13:23:14 +0100

    Enabled block load

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -13,8 +12,11 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        'Illuminate\Routing\Events\RouteMatched' => [
+            'Gzero\Cms\Listeners\BlockLoad',
+        ],
+        'Gzero\Core\Events\RouteMatched' => [
+            'Gzero\Cms\Listeners\BlockLoad',
         ],
     ];
 
