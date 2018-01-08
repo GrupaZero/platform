c430e0f54f48c631dd60df6c04960087a8fcec50
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2018-01-08 09:29:28 +0100

    Added some example codeception tests (#74)

---
+++ tests/unit/ExampleUnitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Codeception\Test\Unit;
+
+class ExampleUnitTest extends Unit
+{
+    /** @test */
+    public function itWorks()
+    {
+        $this->assertEquals(true, true);
+    }
+}
