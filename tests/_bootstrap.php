5e564031da14d58ae229ab063c94183cbcac345e
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2018-02-20 18:10:59 +0100

    Acceptance tests fixes (#86)
    * Skipped acceptance tests by default
    
    * Skipped acceptance tests by default
    
    * Some fixes to gitlab-ci

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,15 +3,15 @@
 
 use Codeception\Lib\Driver\PostgreSql;
 
-if (file_exists(dirname(__DIR__) . '/.env.testing')) {
+if (file_exists(dirname(__DIR__) . '/.env.testing') && !getenv('CI_ACCEPTANCE_RUN')) {
     (new \Dotenv\Dotenv(dirname(__DIR__), '.env.testing'))->load();
 }
 
-$host = env('DB_HOST', 'db_tests_server');
-$port = env('DB_PORT', 5432);
-$dbName = env('DB_DATABASE', 'gzero_cms');
-$user = env('DB_USERNAME', 'gzero_cms');
-$password = env('DB_PASSWORD', '');
+$host = env('DB_HOST', env('DB_HOST', 'db_server'));
+$port = env('DB_PORT', env('DB_PORT', 5432));
+$dbName = env('DB_DATABASE', env('DB_DATABASE', 'gzero_cms'));
+$user = env('DB_USERNAME', env('DB_USERNAME', 'gzero_cms'));
+$password = env('DB_PASSWORD', env('DB_PASSWORD', ''));
 
 $sql = file_get_contents(__DIR__ . '/db/dump.sql');
 // remove C-style comments (except MySQL directives)
@@ -29,11 +29,13 @@ $db->load($sql);
 // We want to save all output files in platform _output dir
 \Codeception\Configuration::$defaultConfig['paths'] = ['output' => __DIR__ . '/_output'];
 
-$dockerHostIp = exec("/sbin/ip route|awk '/default/ { print $3 }'");
-\Codeception\Configuration::$defaultSuiteSettings['modules']['config'] =
-    [
-        'WebDriver' => [
-            'host' => $dockerHostIp
-        ]
-    ];
-
+if (!getenv('CI_ACCEPTANCE_RUN')) {
+    $dockerHostIp = exec("/sbin/ip route|awk '/default/ { print $3 }'");
+    \Codeception\Configuration::$defaultSuiteSettings['modules']['config'] =
+        [
+            'WebDriver' => [
+                'host' => $dockerHostIp, // Docker host ip
+                'url' => 'http://dev.gzero.pl:8080' // We're binding web_server to 8080
+            ]
+        ];
+}
