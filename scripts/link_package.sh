42ee811bd65c234cb932de83ca74753ad9a931c9
Bartero <bartek.krzywania@gmail.com>
2018-03-16 12:42:18 +0100

    ./develop mount|umount npm npm-package

--- scripts/link_package.sh
+++ scripts/link_package.sh
@@ -18,38 +18,45 @@ function contains() {
     return 1
 }
 
+### DEFINITIONS
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PLATFORM_DIR="$(dirname $SCRIPT_DIR)"
 PACKAGES=("core" "admin" "api" "cms" "social" "vanilla-integration")
 OPERATIONS=("mount" "umount")
-OPERATION=$1
-PACKAGE=$2
 
+
+### FIRST VALIDATION
 [ "$#" -ge 2 ] ||
   die "2 argument required, $# provided"
-[ $(contains "${OPERATIONS[@]}" $OPERATION) == "y" ] ||
+
+
+### DEDUCTING INPUT PARAMETERS
+OPERATION=$1
+MODE=composer
+if [ "$2" == npm ] || [ "$2" == composer ]; then
+  MODE=$2
+  shift
+fi
+PACKAGE=$2
+
+
+### VALIDATING INPUT PARAMETERS
+[ $(contains "${OPERATIONS[@]}" $OPERATION) == y ] ||
   die "Operation must be one of (${OPERATIONS[@]}), $OPERATION provided"
-[ $(contains "${PACKAGES[@]}" $PACKAGE) == "y" ] || [ $PACKAGE == "ALL" ] ||
-  die "Package must be one of (${PACKAGES[@]}) or be ALL, $PACKAGE provided"
 
+if [ $MODE == composer ]; then
+  [ $(contains "${PACKAGES[@]}" $PACKAGE) == y ] || [ $PACKAGE == ALL ] ||
+    die "Package must be one of (${PACKAGES[@]}) or be ALL, $PACKAGE provided"
+fi
 
-function do_operation() {
-  OPERATION=$1
-  PACKAGE=$2
+if [ $MODE == npm ] && [ $PACKAGE == ALL ]; then
+  die "Are you sure you work with NPM module named ALL???"
+fi
 
-  if [ $PACKAGE == "admin" ]; then
-    PACKAGE_DIR="$PLATFORM_DIR/../$PACKAGE/public"
-    MOUNT_DIR="$PLATFORM_DIR/public/gzero/$PACKAGE"
-  else
-    PACKAGE_DIR="$PLATFORM_DIR/../$PACKAGE"
-    MOUNT_DIR="$PLATFORM_DIR/vendor/gzero/$PACKAGE"
-  fi
-
-  ${OPERATION} $PACKAGE
-}
 
 function mount() {
-  PACKAGE=$1
+  echo "PACKAGE_DIR: $PACKAGE_DIR"
+  echo "MOUNT_DIR: $MOUNT_DIR"
 
   if [ ! -d "${MOUNT_DIR}" ]; then
     echo -e "The mount dir for \e[91m$PACKAGE\e[0m is not present"
@@ -58,35 +65,50 @@ function mount() {
 
   # Checking if package was mounted before
   if grep -qs "$MOUNT_DIR" /proc/mounts; then
-      echo -e "\e[1mUnmounting previously mounted package \e[91m$PACKAGE\e[0m"
+      echo -e "\e[1mUnmounting previously mounted $MODE package \e[91m$PACKAGE\e[0m"
       sudo umount "$MOUNT_DIR"
   fi
 
   if [ ! -d "${PACKAGE_DIR}" ]; then
-    echo -e "The project \e[91m$PACKAGE\e[0m is not present"
+    echo -e "The $MODE project \e[91m$PACKAGE\e[0m is not present"
     return
   fi
 
-  echo -e "\e[1mMounting package \e[91m$PACKAGE\e[0m"
+  echo -e "\e[1mMounting $MODE package \e[91m$PACKAGE\e[0m"
   sudo mount --bind "$PACKAGE_DIR" "$MOUNT_DIR" && sudo mount -o bind,remount,ro "$MOUNT_DIR"
 }
 
 function umount() {
-  PACKAGE=$1
-
   # Checking if package was mounted before
-  if grep -qs "$PLATFORM_DIR/vendor/gzero/$PACKAGE" /proc/mounts; then
-    echo -e "\e[1mUnmounting package \e[91m$PACKAGE\e[0m"
+  if grep -qs "$MOUNT_DIR" /proc/mounts; then
+    echo -e "\e[1mUnmounting $MODE package \e[91m$PACKAGE\e[0m"
     sudo umount "$MOUNT_DIR"
   else
-    echo -e "The package \e[91m$PACKAGE\e[0m is not mounted"
+    echo -e "The $MODE package \e[91m$PACKAGE\e[0m is not mounted"
   fi
 }
 
-if [ $PACKAGE == "ALL" ]; then
-  for package in ${PACKAGES[@]}; do
-    do_operation $OPERATION $package
+function prepare_operation_parameters() {
+  if [ $MODE == composer ]; then
+    if [ $PACKAGE == "admin" ]; then
+      PACKAGE_DIR="$PLATFORM_DIR/../$PACKAGE/public"
+      MOUNT_DIR="$PLATFORM_DIR/public/gzero/$PACKAGE"
+    else
+      PACKAGE_DIR="$PLATFORM_DIR/../$PACKAGE"
+      MOUNT_DIR="$PLATFORM_DIR/vendor/gzero/$PACKAGE"
+    fi
+  else
+    PACKAGE_DIR="$PLATFORM_DIR/../node_projects/$PACKAGE"
+    MOUNT_DIR="$PLATFORM_DIR/node_modules/$PACKAGE"
+  fi
+}
+
+if [ $MODE == composer ] && [ $PACKAGE == ALL ]; then
+  for PACKAGE in ${PACKAGES[@]}; do
+    prepare_operation_parameters
+      ${OPERATION}
   done
 else
-  do_operation $OPERATION $PACKAGE
+  prepare_operation_parameters
+    ${OPERATION}
 fi
